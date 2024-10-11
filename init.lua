385a423455acff5504cdb5de11c11372342190a1
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2024-10-11 15:11:09 -0400

    add markdown

--- init.lua
+++ init.lua
@@ -264,6 +264,12 @@ require('lazy').setup({
     opts = {},
   },
 
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  },
+
   --   opts = {
   --     signs = {
   --       add = { text = '+' },
